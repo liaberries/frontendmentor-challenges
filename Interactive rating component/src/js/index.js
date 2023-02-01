0ef36ca40d378d41a8d499aac4cefd424522cf7f
Lia Smith <smith.kelia93@gmail.com>
2023-02-01 12:01:30 -0800

    made changes to index.html, main.scss, and index.js

--- Interactive rating component/src/js/index.js
+++ Interactive rating component/src/js/index.js
@@ -2,7 +2,7 @@ const mainContainer = document.querySelector(".main-container")
 const thankYouContainer = document.querySelector(".thank-you-container") 
 const submitButton = document.getElementById("submit-btn")
 const rating = document.getElementById("rating")
-const allRatings = document.querySelectorAll("#btn")
+const allRatings = document.querySelectorAll(".rating-btn")
 
 submitButton.addEventListener("click", () => {
     thankYouContainer.classList.remove("hidden")
