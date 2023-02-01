76d129e23a3730e95b34f8ec862ad84e25198b5f
Lia Smith <smith.kelia93@gmail.com>
2023-02-01 11:50:36 -0800

    updated main.scss, index.html, and index.js

---
+++ Interactive rating component/src/js/index.js
@@ -0,0 +1,17 @@
+const mainContainer = document.querySelector(".main-container")
+const thankYouContainer = document.querySelector(".thank-you-container") 
+const submitButton = document.getElementById("submit-btn")
+const rating = document.getElementById("rating")
+const allRatings = document.querySelectorAll("#btn")
+
+submitButton.addEventListener("click", () => {
+    thankYouContainer.classList.remove("hidden")
+    mainContainer.style.display = "none"
+})
+
+allRatings.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    })
+})
+
